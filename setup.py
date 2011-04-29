629d4be4c1ece61459ffae864e8bf702315914ed
embray <embray@stsci.edu>
2011-04-29 19:52:28 +0000

    * Added the aforementioned stsci.distutils package.  This can be a central    package, with no other dependencies, where all distutils-related stuff can    go (I'd still like to figure out a way to move out that chunk of code in    individual projects' setup.py files for using stsci.distutils, but for now    it stays).
     * In pydrizzle, I moved the setup_hook.py module out of the pydrizzle package
       itself.  This was causing too many problems since pydrizzle's __init__.py
       has some imports that in turn try to import some of pydrizzle's
       dependencies.  Could use a numpy-like hack to prevent execution of code in
       its __init__.py during setup, but not worth it right now.
    
     * Got rid of the defsetup.py hack in all packages that have been updated.
     
    
    
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/branches/setup_refactoring/ndimage@12692 fe389314-cf27-0410-b35b-8c050e845b92

--- setup.py
+++ setup.py
@@ -13,40 +13,40 @@ from ConfigParser import ConfigParser
 import pkg_resources
 
 
-SETUP_REQUIRES = ['d2to1', 'stsci.tools==2.9']
+SETUP_REQUIRES = ['d2to1', 'stsci.distutils==0.2']
 
 
-# This project requires utilities from the stsci.tools package in order to
-# build.  If stsci.tools is not already installed, it may automatically be
+# This project requires utilities from the stsci.distutils package in order to
+# build.  If stsci.distutils is not already installed, it may automatically be
 # downloaded from PyPI.  However, in a full source distribution of
 # stsci_python, it should also be available in a sibling directory to this one,
 # so that should be used instead if possible.
 for requirement in pkg_resources.parse_requirements(SETUP_REQUIRES):
-    if requirement.project_name != 'stsci.tools':
+    if requirement.project_name != 'stsci.distutils':
         continue
 
     try:
-        # See if the required version of stsci.tools is already available
+        # See if the required version of stsci.distutils is already available
         pkg_resources.get_distribution(requirement)
-        has_stsci_tools = True
+        has_stsci_distutils = True
     except (pkg_resources.VersionConflict,
             pkg_resources.DistributionNotFound):
-        has_stsci_tools = False
+        has_stsci_distutils = False
 
-    if has_stsci_tools:
+    if has_stsci_distutils:
         break
 
-    tools_dir = os.path.join(os.path.pardir, 'tools')
+    tools_dir = os.path.join(os.path.pardir, 'distutils')
     setup_cfg = os.path.join(tools_dir, 'setup.cfg')
     if os.path.exists(setup_cfg):
-        # Check the setup.cfg in ../tools; make sure it is in fact for
-        # stsci.tools and that the version is as required
+        # Check the setup.cfg in ../distutils; make sure it is in fact for
+        # stsci.distutils and that the version is as required
         cfg = ConfigParser()
         cfg.read(setup_cfg)
         if not cfg.has_option('metadata', 'name'):
             break
         name = cfg.get('metadata', 'name')
-        if name != 'stsci.tools':
+        if name != 'stsci.distutils':
             break
         if cfg.has_option('metadata', 'version'):
             version = cfg.get('metadata', 'version')
@@ -55,15 +55,15 @@ for requirement in pkg_resources.parse_requirements(SETUP_REQUIRES):
         if version not in requirement:
             break
 
-        # stsci.tools will almost always have packages_root = lib, but better
-        # not to make assumptions
+        # stsci.distutils will almost always have packages_root = lib, but
+        # better not to make assumptions
         if cfg.has_option('files', 'packages_root'):
             location = os.path.join(tools_dir,
                                     cfg.get('files', 'packages_root'))
         else:
             location = tools_dir
 
-        # If a different version of stsci.tools is already in the current
+        # If a different version of stsci.distutils is already in the current
         # distribution list, remove it and add a distribution pointing to our
         # source-local copy
         dist = pkg_resources.Distribution(
