ef93b74eaad2faf05d6a09f3ec0c39aeced314d6
embray <embray@stsci.edu>
2011-05-19 15:12:35 +0000

    It ocurred to me that the last source release of stsci_python did include pywcs and pysynphot, so they should not be excluded, I guess.  coords was not included, though maybe it should be?  Worth asking about...
    git-svn-id: http://svn.stsci.edu/svn/ssb/stsci_python/stsci_python/branches/setup_refactoring/ndimage@12883 fe389314-cf27-0410-b35b-8c050e845b92

--- lib/stsci/ndimage/__init__.py
+++ lib/stsci/ndimage/__init__.py
@@ -46,7 +46,11 @@ except:
     __svn_version__ = 'Unable to determine SVN revision'
     __full_svn_info__ = __svn_version__
 
-import pytools.tester
 
-def test(*args,**kwds):
-    pytools.tester.test(modname=__name__, *args, **kwds)
+try:
+    import stsci.tools.tester
+
+    def test(*args,**kwds):
+        stsci.tools.tester.test(modname=__name__, *args, **kwds)
+except ImportError:
+    pass
